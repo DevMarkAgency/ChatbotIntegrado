f13bba9f35758950f688433a2484503968b6b900
Daniel Navarro <navarro@sextubecol.online>
2024-09-25 00:40:12 -0500

    update v4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./api/chat');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Servidor de chatbot ejecutándose en http://localhost:${port}`);
